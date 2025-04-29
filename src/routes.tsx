e636a246eb4128375c207397eea14cca97fa923f
JLannoo <julianlannoo@gmail.com>
2025-04-29 12:18:35 -0300

    remove: delete unused routes file

--- src/routes.tsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from 'react';
-import { Navigate, RouteProps } from 'react-router';
-
-const Index = lazy(() => import('./pages/Index/Index'));
-
-export const routes: RouteProps[] = [
-  { path: '*', element: <Navigate to="/" /> },
-  { path: '/', element: <Index /> },
-];
